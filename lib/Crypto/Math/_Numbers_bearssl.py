afaa4bce543620bb82a0a754f56a20a76bf3081e
Helder Eijs <helderijs@gmail.com>
2017-09-28 17:11:06 +0200

    Experiment with BearSSL [WIP]
    [skipci]

--- lib/Crypto/Math/_Numbers_bearssl.py
+++ lib/Crypto/Math/_Numbers_bearssl.py
@@ -8,12 +8,54 @@ from Crypto.Util.number import long_to_bytes, bytes_to_long
 from Crypto.Util._raw_api import ffi
 
 _bearssl_defs = """
-    void br_i31_decode(uint32_t *x, const void *src, size_t len);
-    void br_i31_encode(void *dst, size_t len, const uint32_t *x);
+uint32_t br_i31_iszero(const uint32_t *x);
+uint32_t br_i31_add(uint32_t *a, const uint32_t *b, uint32_t ctl);
+uint32_t br_i31_sub(uint32_t *a, const uint32_t *b, uint32_t ctl);
+uint32_t br_i31_bit_length(uint32_t *x, size_t xlen);
+void br_i31_decode(uint32_t *x, const void *src, size_t len);
+uint32_t br_i31_decode_mod(uint32_t *x,
+	const void *src, size_t len, const uint32_t *m);
+void br_i31_rshift(uint32_t *x, int count);
+void br_i31_reduce(uint32_t *x, const uint32_t *a, const uint32_t *m);
+void br_i31_decode_reduce(uint32_t *x,
+	const void *src, size_t len, const uint32_t *m);
+void br_i31_muladd_small(uint32_t *x, uint32_t z, const uint32_t *m);
+void br_i31_encode(void *dst, size_t len, const uint32_t *x);
+uint32_t br_i31_ninv31(uint32_t x);
+void br_i31_montymul(uint32_t *d, const uint32_t *x, const uint32_t *y,
+	const uint32_t *m, uint32_t m0i);
+void br_i31_to_monty(uint32_t *x, const uint32_t *m);
+void br_i31_from_monty(uint32_t *x, const uint32_t *m, uint32_t m0i);
+void br_i31_modpow(uint32_t *x, const unsigned char *e, size_t elen,
+	const uint32_t *m, uint32_t m0i, uint32_t *t1, uint32_t *t2);
+uint32_t br_i31_modpow_opt(uint32_t *x, const unsigned char *e, size_t elen,
+	const uint32_t *m, uint32_t m0i, uint32_t *tmp, size_t twlen);
+void br_i31_mulacc(uint32_t *d, const uint32_t *a, const uint32_t *b);
 """
 
 _raw_bearssl = load_pycryptodome_raw_lib("Crypto.Util._bearssl", _bearssl_defs)
 
+# With i31, the number is represented with 31-bit limbs via an array of 32-bit words.
+#
+#
+#
+# The first word of the array is the "announced bit length" (i.e. the # of meaningful bits):
+# - the upper 27 bits are the numer of full limbs
+# - the lower 5 bits are the residual bits in the last limb - if 0 there is no last limb
+#
+# Example of total array size for:
+# - a 0 bit number:     1 word
+# - a 1 bit number:     2 words
+# - a 32 bit number:    2 words
+# - a 33 bit number:    3 words
+#
+# For an array X, the idiom (X[0] + 31) >> 5 is used to compute the number of limbs.
+# The total array length is clearly (X[0] + 31) >> 5 + 1.
+
+_MAX_BITSIZE = 4096
+_MAX_SIZE = 1 << _MAX_BITSIZE - 1
+_MAX_NR_WORDS = _MAX_BITSIZE // 31 + 2
+_MAX_BYTESIZE = _MAX_BITSIZE // 8 + 1
 
 class Integer(object):
 
@@ -23,23 +65,17 @@ class Integer(object):
         if isinstance(value, float):
             raise ValueError("A floating point type is not a natural number")
 
-        if value < 0 or value >= 2**4096:
+        if value < 0 or value > _MAX_SIZE:
             raise ValueError("Integer not in valid range")
 
-        # We only support integer up to 4096 bits (133+1 31-bit words)
-        self._i31 = ffi.new("uint32_t[]", 134)
+        self._i31 = ffi.new("uint32_t[]", _MAX_NR_WORDS)
 
-        big_endian = long_to_bytes(value)
-        _raw_bearssl.br_i31_decode(self._i31, big_endian, len(big_endian))
+        buf = long_to_bytes(value)
+        _raw_bearssl.br_i31_decode(self._i31, buf, len(buf))
 
     # Conversions
     def __int__(self):
-
-        # Enough room for 4096 bits
-        # BearSSL will zero fill if too big
-        buf = create_string_buffer(512)
-        _raw_bearssl.br_i31_encode(buf, len(buf), self._i31)
-        return bytes_to_long(get_raw_buffer(buf))
+        return bytes_to_long(self.to_bytes())
 
     def __str__(self):
         return str(int(self))
@@ -65,10 +101,12 @@ class Integer(object):
           provided and the length of the byte string would exceed it.
         """
 
-        raise NotImplementedError("Not implemented yet")
+        buf = create_string_buffer(self.size_in_bytes())
+        _raw_bearssl.br_i31_encode(buf, len(buf), self._i31)
+        return get_raw_buffer(buf)
 
-    @staticmethod
-    def from_bytes(byte_string):
+    @classmethod
+    def from_bytes(cls, byte_string):
         """Convert a byte string into a number.
 
         :Parameters:
@@ -79,7 +117,10 @@ class Integer(object):
           The ``Integer`` object carrying the same value as the input.
         """
 
-        raise NotImplementedError("Not implemented yet")
+        self = cls.__new__(cls)
+        self._i31 = ffi.new("uint32_t[]", _MAX_NR_WORDS)
+        _raw_bearssl.br_i31_decode(self._i31, byte_string, len(byte_string`))
+        return self
 
     # Relations
     def _apply_and_return(self, func, term):
@@ -88,36 +129,36 @@ class Integer(object):
         return func(self._mpz_p, term._mpz_p)
 
     def __eq__(self, term):
-        if not isinstance(term, (Integer, int, long)):
-            return False
-        raise NotImplementedError("Not implemented yet")
+        return int(self) == int(term)
 
     def __ne__(self, term):
-        if not isinstance(term, (Integer, int, long)):
-            return True
-        raise NotImplementedError("Not implemented yet")
+        return int(self) != int(term)
 
     def __lt__(self, term):
-        raise NotImplementedError("Not implemented yet")
+        return int(self) < int(term)
 
     def __le__(self, term):
-        raise NotImplementedError("Not implemented yet")
+        return int(self) <= int(term)
 
     def __gt__(self, term):
-        raise NotImplementedError("Not implemented yet")
+        return int(self) > int(term)
 
     def __ge__(self, term):
-        raise NotImplementedError("Not implemented yet")
+        return int(self) >= int(term)
 
     def __nonzero__(self):
-        raise NotImplementedError("Not implemented yet")
+        return _raw_bearssl.br_i31_iszero(self._i31) == 0
 
     def is_negative(self):
         raise NotImplementedError("Not implemented yet")
 
     # Arithmetic operations
     def __add__(self, term):
-        raise NotImplementedError("Not implemented yet")
+        result = cls.__new__(cls)
+        result._i31 = ffi.new("uint32_t[]", _MAX_NR_WORDS)
+        ffi.memmove(result._i31, self._i31, MAX_NR_WORDS * 4)
+        _raw_bearssl.br_i31_add(result._i31, term._i31, 1);
+        return result
 
     def __sub__(self, term):
         raise NotImplementedError("Not implemented yet")
@@ -166,10 +207,15 @@ class Integer(object):
         raise NotImplementedError("Not implemented yet")
 
     def __rshift__(self, pos):
-        raise NotImplementedError("Not implemented yet")
+        result = cls.__new__(cls)
+        result._i31 = ffi.new("uint32_t[]", _MAX_NR_WORDS)
+        ffi.memmove(result._i31, self._i31, MAX_NR_WORDS * 4)
+        result >>= pos
+        return result
 
     def __irshift__(self, pos):
-        raise NotImplementedError("Not implemented yet")
+        _raw_bearssl.br_i31_rshift(self._i31, pos)
+        return self
 
     def __lshift__(self, pos):
         raise NotImplementedError("Not implemented yet")
@@ -192,11 +238,16 @@ class Integer(object):
 
     def size_in_bits(self):
         """Return the minimum number of bits that can encode the number."""
-        raise NotImplementedError("Not implemented yet")
+
+        if self._i31[0] == 0:
+            return 1
+        limbs = (self._i31[0] >> 5) + 1
+        res = _raw_bearssl.br_i31_bit_length(self.i31 + 1, limbs)
+        return res
 
     def size_in_bytes(self):
         """Return the minimum number of bytes that can encode the number."""
-        raise NotImplementedError("Not implemented yet")
+        return (self.size_in_bits() - 1) // 8 + 1
 
     def is_perfect_square(self):
         raise NotImplementedError("Not implemented yet")
@@ -211,7 +262,8 @@ class Integer(object):
 
     def set(self, source):
         """Set the Integer to have the given value"""
-        raise NotImplementedError("Not implemented yet")
+        buf = long_to_bytes(value)
+        _raw_bearssl.br_i31_decode(self._i31, buf, len(buf))
 
     def inplace_inverse(self, modulus):
         """Compute the inverse of this number in the ring of
